7e9c8314298f685a1ea5515072c9c87eac349652
github2764 <78102700+github2764@users.noreply.github.com>
2025-03-21 18:58:53 +0530

    Update close-9090-port.sh

--- close-9090-port.sh
+++ close-9090-port.sh
@@ -1,2 +1,6 @@
+#You can remove the cockpit service from the firewall like this:
 firewall-cmd --remove-service=cockpit --permanent
 firewall-cmd --reload
+
+#If you’re not using Cockpit (a web-based admin tool), you can also disable it entirely as below
+#systemctl disable --now cockpit.socket
