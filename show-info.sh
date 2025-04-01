b99f7b22617c2e9318614a4fc0329d85de7252b1
github2764 <78102700+github2764@users.noreply.github.com>
2025-04-01 12:37:14 +0530

    Update show-info.sh

--- show-info.sh
+++ show-info.sh
@@ -7,3 +7,8 @@ sudo firewall-cmd --state
 sudo firewall-cmd --list-services
 sudo firewall-cmd --get-active-zones
 sudo firewall-cmd --zone=public --list-all
+
+#in order to get the list of ports open by firewalld you have to run --list-all and then 
+#if it shows ports(if you used add port command) and services if you used --add-service
+#you have to then run sudo firewall-cmd --info-service=cockpit to get to see cockpit port
+#so effectively 2 commands to get to see all ports 
