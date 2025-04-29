b04ae5a553c9fdbc30b6651366d2385f5086a08e
github2764 <78102700+github2764@users.noreply.github.com>
2025-04-29 08:59:20 +0530

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -5,6 +5,11 @@ sudo systemctl enable --now firewalld
 #--now will actually start the socket right away. no need to start cockpit.service if you want on demand socket 
 #activated cockpit-service for efficiency
 sudo systemctl enable --now cockpit.socket
+
+to stop
+sudo systemctl stop --now cockpit.socket/service
+sudo systemctl disable --now cockpit.socket/service
+
 #in order to login into cockpit via browser we need login and pass
 sudo passwd rocky
 
